96b677c863c5161f372e016efb1b27a76dedc305
Yogesh SA <yogesh.5prog@gmail.com>
2025-01-07 22:35:38 +0530

    Done bug

--- app/api/fetchProducts/route.js
+++ app/api/fetchProducts/route.js
@@ -1,18 +1,19 @@
-import connectDb from "@/lib/database/db"
-import Product from "@/lib/Schema/productSchema"
+import connectDb from "@/lib/database/db";
+import Product from "@/lib/Schema/productSchema";
 
-
-export async function GET(req, {params}) {
+export async function GET(req, { params }) {
     try {
-        await connectDb()
-        const url = new URL(req.url)
-        const category = url.searchParams.get('category')
-        if(category){
-            const data = await Product.find({productCategory: category})
-            return new Response(JSON.stringify(data), { status: 200 })
+        await connectDb();
+        const url = new URL(req.url);
+        const category = url.searchParams.get('category');
+
+        if (category) {
+            const data = await Product.find({ productCategory: category });
+            return new Response(JSON.stringify(data), { status: 200 });
         }
-        return new Response(JSON.stringify({ message: "Product not found" }), { status: 500 })
+
+        return new Response(JSON.stringify({ message: "Product not found" }), { status: 500 });
     } catch (error) {
-        return new Response( { status : 500}, JSON.stringify({ message : error.message }))
+        return new Response(JSON.stringify({ message: error.message }), { status: 500 });
     }
 }
